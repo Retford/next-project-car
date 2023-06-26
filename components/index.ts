36dfa9a9ac55a441d61953b3e83b6fd422dec276
Retford <retfordjb@gmail.com>
2023-06-26 03:22:52 -0500

    Avance 2

---
+++ components/index.ts
@@ -0,0 +1,24 @@
+import Hero from "./Hero";
+import CustomButton from './CustomButton';
+import Footer from './Footer';
+import NavBar from './NavBar';
+import CustomFilter from './CustomFilter';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CardDetails from './CardDetails';
+import ShowMore from './ShowMore';
+
+
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CardDetails,
+  ShowMore,
+  Footer
+}
